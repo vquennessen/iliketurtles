b44a24d28100a5afb999c61c5e357a79e70ddbe9
vquennessen <viquennessen@gmail.com>
2021-12-31 16:51:41 -0500

    created separate function for calculating confidence of detecting number of sires given different breeding forms (i.e. random, exponential, dominating)

---
+++ code/eggs_to_sample.R
@@ -0,0 +1,116 @@
+### function for number of eggs to sample to determine the number of sires
+
+
+# population parameters
+n_eggs         <- 100    # number of eggs per nest
+max_hatchlings <- 40     # maximum number of hatchlings that can be sampled
+max_males      <- 7      # maximum number of males females can mate with
+
+eggs_to_sample <- function(n_eggs = 100, 
+                           max_hatchlings = 96, 
+                           max_males = 7, 
+                           breeding, 
+                           n_sims = 100000) {
+  
+  max_eggs <- min(max_hatchlings, n_eggs) 
+  
+  
+  # pre-allocate data frame
+  DF <- data.frame(Males = rep(1:max_males, each = (max_eggs - 1)), 
+                   Sample_size = rep(2:max_eggs, times = max_males), 
+                   Proportion_correct = rep(NA, dim = max_males*(max_eggs - 1)))
+  
+  
+  # for each number of males that contribute to a nest:
+  for (i in 1:max_males) {
+    
+    if (i > 1) {
+      # set contributions per males
+      if (breeding == 'dominate') {
+        MC <- 0.8
+        contributions <- c(MC, rep((1 - MC)/(i - 1), (i - 1)))
+      } else if (breeding == 'exponential') {
+        MC <- 0.5
+        contributions <- 0.5^c(1:(i-1))
+        contributions <- c(contributions, contributions[i-1])
+      } else if (breeding == 'random') {
+        contributions <- rep(1/i, i)
+      }
+    } else { contributions <- c(1)}
+    
+    # proportion_correct array
+    prop_correct <- rep(NA, n_sims)
+    
+    # for each sample size
+    for (j in 2:max_eggs) {
+      
+      # pre-allocate correct identifications of number of males
+      correct <- rep(NA, n_sims)
+      
+      for (k in 1:n_sims) {
+        
+        # simulate male contributions to nest
+        nest <- sample(x = 1:i, 
+                       size = n_eggs, 
+                       replace = TRUE,
+                       prob = contributions)
+        
+        # take samples of size k
+        samples <- sample(x = nest, 
+                          size = j, 
+                          replace = FALSE)
+        
+        # correct allocation of number of males?
+        correct[k] <- length(unique(samples)) == i
+        
+      }
+      
+      # calculate index in data frame
+      index <- (i - 1)*(max_eggs - 1) + j - 1
+      
+      # stick proportion in data frame
+      DF$Proportion_correct[index] <- mean(correct)
+    }
+    
+  }
+  
+  #### plot results
+  
+  # color-blind friendly color palette
+  colors <- viridis(max_males)
+  
+  # plot results
+  fig1 <- ggplot(DF, aes(x = Sample_size, y = Proportion_correct, 
+                         col = as.factor(Males))) +
+    geom_path(lwd = 1) +
+    labs(col = 'Number \n of Males') +
+    scale_color_manual(values = colors) +
+    ylab('Proportion Correct') +
+    xlab('Eggs Sampled') +
+    geom_vline(xintercept = 32, linetype = 2)
+  
+  # save results to image file
+  ggsave(plot = last_plot(), 
+         filename = paste(breeding, '_fig1_eggs_to_sample.png', sep = ''),
+         path = 'C://Users/vique/Documents/Projects/iliketurtles/figures',
+         width = 6, height = 3)
+  
+  
+  # What's our confidence if we sample 32 percent of the eggs?
+  
+  DF32 <- subset(DF, Sample_size == 32)
+  DF32 <- DF32[, c(1, 3)]
+  names(DF32) <- c('Number of Males', 'Confidence')
+  row.names(DF32) <- NULL
+  
+  png(filename = paste('C://Users/vique/Documents/Projects/iliketurtles/figures/', 
+                       breeding, '_ss32_conf_table.png', sep = ''), 
+      width = 200, height = 200)
+  grid.table(DF32)
+  dev.off()
+  
+  output <- list(fig1, DF32)
+  
+  return(output)
+  
+}
