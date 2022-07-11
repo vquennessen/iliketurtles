2e1836639ef4366077fdb56715644f0fad846961
vquennessen <viquennessen@gmail.com>
2022-07-11 15:34:25 +0200

    added section for calculating beta distribution for hatching success for nests

--- code/extract_FdN_data.R
+++ code/extract_FdN_data.R
@@ -6,6 +6,8 @@ library(readr)
 library(ggplot2)
 library(reshape)
 
+##### load in and clean data ###################################################
+
 # set working directory
 setwd('~/Projects/iliketurtles/code')
 
@@ -42,9 +44,10 @@ all_seasons <- rbind(new_S1, new_S2, new_S3)
 # nests
 nests <- all_seasons %>%
   filter(TIPO_REG == 'CD') %>%
-  mutate(Female = NA)
+  mutate(Female = NA) %>%
+  mutate(Hatching_success = VIVOS / OVOS_TOT)
 
-# initialise females reference dataframe with column names and first female
+##### initialise females reference DF with column names and first female #######
 
 # first marcas
 marcas <- as.character(as.vector(nests[7, c(3, 4, 5, 6, 11)]))
@@ -106,30 +109,6 @@ save(females, file = '../data/females.Rdata')
 
 ##### eggs per nest ############################################################
 
-# overall mean
-mean(nests_egg_counts$OVOS_TOT) 
-# 102.2625
-
-# overall SD
-sd(nests_egg_counts$OVOS_TOT) 
-# 21.42993
-
-# average for each season
-mean_eggs <- nests %>%
-  group_by(Season) %>%
-  summarise(Mean = mean(OVOS_TOT, na.rm = TRUE))
-# season 1: 101
-# season 2: 101
-# season 3: 103
-
-# SD for each season
-sd_eggs <- nests %>%
-  group_by(Season) %>%
-  summarise(SD = sd(OVOS_TOT, na.rm = TRUE))
-# season 1: 27.1
-# season 2: 18.8
-# season 3: 20.9
-
 # remove nests with no total egg counts
 nests_egg_counts <- nests %>%
   filter(!is.na(OVOS_TOT)) 
@@ -155,5 +134,231 @@ ggplot(data = nests_egg_counts, aes(x = OVOS_TOT,
        y = 'Density \n',
        fill = 'Season')
 
+# overall mean
+mean(nests_egg_counts$OVOS_TOT) 
+# 102.2625
+
+# overall median
+median(nests_egg_counts$OVOS_TOT) 
+# 101.5
+
+# overall SD
+sd(nests_egg_counts$OVOS_TOT) 
+# 21.42993
+
+# average for each season
+mean_eggs <- nests %>%
+  group_by(Season) %>%
+  summarise(Mean = mean(OVOS_TOT, na.rm = TRUE))
+# season 1: 101
+# season 2: 101
+# season 3: 103
+
+# median for each season
+median_eggs <- nests %>%
+  group_by(Season) %>%
+  summarise(Median = median(OVOS_TOT, na.rm = TRUE))
+# season 1: 98
+# season 2: 101
+# season 3: 103
+
+# SD for each season
+sd_eggs <- nests %>%
+  group_by(Season) %>%
+  summarise(SD = sd(OVOS_TOT, na.rm = TRUE))
+# season 1: 27.1
+# season 2: 18.8
+# season 3: 20.9
+
+# SD and mean for all seasons minus season one
+eggs_S2_S3 <- nests %>%
+  filter(Season != 2020) 
+
+sd(eggs_S2_S3$OVOS_TOT, na.rm = TRUE)
+# 20.1344
+
+mean(eggs_S2_S3$OVOS_TOT, na.rm = TRUE)
+# 102.4625
+
+##### hatching success #########################################################
+
+# remove nests with no hatching success calculated
+nests_HS <- nests %>%
+  filter(!is.na(Hatching_success)) 
+
+# scatterplot of eggs per nest colored by season
+ggplot(data = nests_HS, aes(x = as.factor(Season), 
+                            y = Hatching_success)) + 
+  geom_dotplot(binaxis = 'y', stackdir = 'center', dotsize = 0.4) + 
+  geom_boxplot(alpha = 0.5, 
+               aes(fill = as.factor(Season))) +
+  labs(x = '\n Season', 
+       y = 'Hatching success \n') + 
+  theme(legend.position = 'none')
+
+# overlapping density plot, colored by season 
+ggplot(data = nests_HS, aes(x = Hatching_success, 
+                            fill = as.factor(Season), 
+                            alpha = 0.5)) + 
+  geom_density(aes(group = as.factor(Season), 
+                   alpha = 0.5)) +
+  guides(alpha = 'none') +
+  labs(x = '\n Hatching Success', 
+       y = 'Density \n',
+       fill = 'Season')
+
+# overall mean
+mean(nests_egg_counts$Hatching_success) 
+# 0.8150182
+
+# overall median
+median(nests_egg_counts$Hatching_success) 
+# 0.8849624
+
+# overall SD
+sd(nests_egg_counts$Hatching_success) 
+# 0.191776
+
+# average for each season
+mean_HS <- nests %>%
+  group_by(Season) %>%
+  summarise(Mean = mean(Hatching_success, na.rm = TRUE))
+# season 1: 0.835
+# season 2: 0.782
+# season 3: 0.829
+
+# median for each season
+median_HS <- nests %>%
+  group_by(Season) %>%
+  summarise(Median = median(Hatching_success, na.rm = TRUE))
+# season 1: 0.86 
+# season 2: 0.847
+# season 3: 0.903
+
+# SD for each season
+sd_HS <- nests %>%
+  group_by(Season) %>%
+  summarise(SD = sd(Hatching_success, na.rm = TRUE))
+# season 1: 0.119
+# season 2: 0.185
+# season 3: 0.212
+
+# SD for all seasons minus season one
+sd(HS_S2_S3$Hatching_success, na.rm = TRUE)
+# 0.2032503
+
+# mean for all seasons minus season one
+HS_S2_S3 <- nests %>%
+  filter(Season != 2020) 
+mu <- mean(HS_S2_S3$Hatching_success, na.rm = TRUE)
+# 0.8110467
+
+# variance for all seasons minus season one
+sigma_sq <- var(HS_S2_S3$Hatching_success, na.rm = TRUE)
+# 0.04131066
+
+# calculate beta distribution parameters
+alpha <- mu*((mu*(1 - mu))/sigma_sq - 1)
+alpha
+beta <- alpha / mu - alpha
+beta
+
+
 ##### number of nests ##########################################################
+
+# nests with females ID'd
+nests_females <- nests %>%
+  filter(!is.na(Female))
+
+# number of nests for each female, including those where eggs were not counted
+nest_counts <- count(nests_females, Female, Season)
+
+# scatterplot of number of nests vs. female number
+ggplot(data = nest_counts, aes(x = Female, y = n, col = as.factor(Season))) +
+  geom_point() + 
+  labs(y = 'Number of nests', 
+       color = 'Season')
+
+# excluding first season
+nests_females_noS1 <- nests %>%
+  filter(!is.na(Female)) %>%
+  filter(Season != 2020)
+
+# number of nests
+nest_counts_noS1 <- count(nests_females_noS1, Female)
+
+# scatterplot of number of nests vs. female number without Season 1
+ggplot(data = nest_counts_noS1, aes(x = Female, y = n)) +
+  geom_point()
+
+# boxplot by season
+# scatterplot of eggs per nest colored by season
+ggplot(data = nest_counts, aes(x = as.factor(Season), y = n)) + 
+  geom_dotplot(binaxis = 'y', stackdir = 'center', dotsize = 0.4) + 
+  geom_boxplot(alpha = 0.5, 
+               aes(fill = as.factor(Season))) +
+  labs(x = '\n Season', 
+       y = 'Number of nests \n') + 
+  theme(legend.position = 'none')
+
+# overlapping density plot, colored by season 
+ggplot(data = nest_counts, aes(x = n, fill = as.factor(Season), alpha = 0.5)) + 
+  geom_density(aes(group = as.factor(Season), 
+                   alpha = 0.5)) +
+  guides(alpha = 'none') +
+  labs(x = '\n Number of nests', 
+       y = 'Density \n',
+       fill = 'Season')
+
+# overall mean
+mean(nest_counts$n) 
+# 4.464912
+
+# overall median
+median(nest_counts$n) 
+# 5
+
+# overall SD
+sd(nest_counts$n) 
+# 2.053186
+
+# average for each season
+mean_nests <- nest_counts %>%
+  group_by(Season) %>%
+  summarise(Mean = mean(n, na.rm = TRUE))
+# season 1: 3.73
+# season 2: 4.36
+# season 3: 5.34
+
+# median for each season
+median_nests <- nest_counts %>%
+  group_by(Season) %>%
+  summarise(Median = median(n, na.rm = TRUE))
+# season 1: 3
+# season 2: 5
+# season 3: 5
+
+# SD for each season
+sd_nests <- nest_counts %>%
+  group_by(Season) %>%
+  summarise(SD = sd(n, na.rm = TRUE))
+# season 1: 2.02
+# season 2: 1.81
+# season 3: 1.96
+
+# SD for all seasons minus season one
+nests_S2_S3 <- nest_counts %>%
+  filter(Season != 2020) 
+sd(nests_S2_S3$n, na.rm = TRUE)
+# 1.94
+
+# mean for all seasons minus season one
+mean(nests_S2_S3$n, na.rm = TRUE)
+# 4.942029
+
+# median for all seasons minus season one
+median(nests_S2_S3$n, na.rm = TRUE)
+# 5
+
+##### remigration interval for females #########################################
 
