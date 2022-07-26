75cc1ba69d7d7854ac51d62fbc428e7bbd8bceb3
vquennessen <viquennessen@gmail.com>
2022-07-25 20:12:45 -0700

    started script for population dynamics

--- code/pop_dynamics.R
+++ code/pop_dynamics.R
@@ -1,24 +1,8 @@
-# base population dynamics model
+# population dynamics
 
-###### parameters ##############################################################
-
-# life history parameters
-max_age <- X             # maximum age  
-
-# model parameters
-y <- 200                 # number of years
-
-##### initialize arrays ########################################################
-
-# numbers at age; dimensions 2 (sex) * maximum age * number of years
-N <- array(rep(NA, 2*max_age*y))
-
-# extract temperature estimates from IPCC prediction scenarios
-
-##### model ####################################################################
-
-# calculate number of hatchlings given male and female adults
-
-# adjust hatchling mortality and sex ratio given predicted temperature
-
-# step rest of population forward one year given natural mortality estimates
+pop_dynamics <- function(survival) {
+  
+  # annual survival
+  
+  
+}
