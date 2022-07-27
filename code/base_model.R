73bce1a893e498b52cd5e35ec1b6ea7457640716
vquennessen <viquennessen@gmail.com>
2022-07-26 21:09:32 -0700

    added female and male survival rates and life stages to base_model.R

--- code/base_model.R
+++ code/base_model.R
@@ -4,18 +4,25 @@
 setwd("~/Projects/iliketurtles/code")
 
 # turtle demographic parameters
-max_age <- 85
+max_age <- 85                                         # lifespan
+F_survival_years <- c(1, 2, 7, 12, 1)                 # years per stage - F
+F_survival_values <- c(0.35, 0.8, 0.85, 0.85, 0.929)  # survival per stage - F
+M_survival_years <- c(1, 2, 7, 12, 1)                 # years per stage - M
+M_survival_values <- c(0.35, 0.8, 0.85, 0.85, 0.490)  # survival per stage - M
 
 # model parameters
-years <- 200
-scenarios <- c('Good', 'Same', 'Bad')
-no_scenarios <- length(scenarios)
+years <- 200                                      # years to run model  
+scenarios <- c('Good', 'Same', 'Bad')             # climate scenarios
+no_scenarios <- length(scenarios)                 # number of climate scenarios
 
 # source initialized arrays
-init_output <- initialize_arrays(max_age, years, no_scenarios)
+init_output <- initialize_arrays(max_age, years, no_scenarios, 
+                                 F_survival_years, F_survival_values, 
+                                 M_survival_years, M_survival_values)
 
-N <- init_output[[1]]          # population size array
-survival <- init_output[[2]]   # vector of survival values
+N <- init_output[[1]]            # population size array
+F_survival <- init_output[[2]]   # vector of survival values - females
+M_survival <- init_output[[3]]   # vector of survival values - males
 
 # population dynamics
 
