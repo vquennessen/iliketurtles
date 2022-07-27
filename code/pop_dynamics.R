1db2e5a7c926bdc26e69d2abc6973bd7a3b1a00e
vquennessen <viquennessen@gmail.com>
2022-07-26 21:09:58 -0700

    started pop_dynamics script and did survival calculations for males and females

--- code/pop_dynamics.R
+++ code/pop_dynamics.R
@@ -1,8 +1,11 @@
 # population dynamics
 
-pop_dynamics <- function(survival) {
+pop_dynamics <- function(F_survival, M_survival, t) {
   
-  # annual survival
+  # annual survival - females
+  N[1, , t + 1, s] <- F_survival * N[1, , t, s]
   
+  # annual survival - males
+  N[2, , t + 1, s] <- M_survival * N[2, , t, s]
   
 }
