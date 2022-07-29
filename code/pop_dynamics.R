a12ac42ad8639b1c536e4eb6875b230f3f17e6d7
vquennessen <viquennessen@gmail.com>
2022-07-29 15:15:19 -0700

    updated pop_dynamics function to incorporate loops for beta function and climate change scenario

--- code/pop_dynamics.R
+++ code/pop_dynamics.R
@@ -2,11 +2,21 @@
 
 pop_dynamics <- function(F_survival, M_survival, t) {
   
-  # annual survival - females
-  N[1, , t + 1, s] <- F_survival * N[1, , t, s]
-  
-  # annual survival - males
-  N[2, , t + 1, s] <- M_survival * N[2, , t, s]
+  # for each mating function beta value
+  for (b in 1:no_betas) {
+    
+    # for each climate change scenario
+    for (s in 1:no_scenarios) {
+      
+      # annual survival - females
+      N[1, , t + 1, , ] <- F_survival * N[1, , t, b, s]
+      
+      # annual survival - males
+      N[2, , t + 1, , ] <- M_survival * N[2, , t, b, s]
+      
+    }
+    
+  }
   
   # output
   output <- list(N)
