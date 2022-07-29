5f3cb984a14d9902dc000a7476cb6c7fc6561338
vquennessen <viquennessen@gmail.com>
2022-07-29 15:14:54 -0700

    started reproduction function

---
+++ code/reproduction.R
@@ -0,0 +1,22 @@
+# reproduction
+
+reproduction <- function(no_betas, betas, temp_mu, temp_var, pivotal_temp, 
+                         N, t) {
+
+  # for each potential mating function
+  for (b in 1:no_betas) {
+    
+    beta <- betas[b]
+    
+    # for each temperature scenario 
+    for (t in 1:length(temp_mu)) {
+      
+      
+      
+      
+    }
+    
+    
+  }
+  
+}
