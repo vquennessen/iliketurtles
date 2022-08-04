33a1a446ffdbbc9f813750bd5757e2266fcbe3db
vquennessen <viquennessen@gmail.com>
2022-08-03 18:25:59 -0700

    working on base_model

--- code/base_model.R
+++ code/base_model.R
@@ -5,31 +5,37 @@ setwd("~/Projects/iliketurtles/code")
 
 ##### parameters ###############################################################
 
-# turtle demographic
+# turtle demographics
 max_age <- 85                                         # lifespan
 F_survival_years <- c(1, 2, 7, 12, 1)                 # years per stage - F
 F_survival_values <- c(0.35, 0.8, 0.85, 0.85, 0.929)  # survival per stage - F
 M_survival_years <- c(1, 2, 7, 12, 1)                 # years per stage - M
 M_survival_values <- c(0.35, 0.8, 0.85, 0.85, 0.490)  # survival per stage - M
-mf_beta <- c(1, 10, 100)                              # mating function beta
-no_betas <- length(mf_beta)                           # num. of mating functions
-pivotal_temp <- 29.3
+betas <- c(1, 10, 100)                                # mating function beta
+pivotal_temp <- 29.3                                  # pivotal temperature 
+TRT <- c(27.6, 31.4)                                  # TR temperatures
+
+# climate data
+temp_mu <- x
+temp_sd <- y
 
 # model parameters
 years <- 200                                      # years to run model  
 scenarios <- c('Same')                            # climate scenarios
-no_scenarios <- length(scenarios)                 # number of climate scenarios
 
 
 ##### source initialized arrays ################################################
 
-init_output <- initialize_arrays(max_age, years, no_betas, no_scenarios, 
+init_output <- initialize_arrays(max_age, years, betas, scenarios, 
+                                 pivotal_temp, TRT, temp_mu, temp_sd, 
                                  F_survival_years, F_survival_values, 
                                  M_survival_years, M_survival_values)
 
-N <- init_output[[1]]            # population size array
-F_survival <- init_output[[2]]   # vector of survival values - females
-M_survival <- init_output[[3]]   # vector of survival values - males
+temperatures <- init_output[[1]] # temperatures across climate scenarios
+prop_male <- init_output[[2]]    # proportion male by temperature 
+N <- init_output[[2]]            # population size array
+F_survival <- init_output[[3]]   # vector of survival values - females
+M_survival <- init_output[[4]]   # vector of survival values - males
 
 ##### model ####################################################################
 
