2c55b26721dc7f846aa3e2097fc719dafdb28bb2
vquennessen <viquennessen@gmail.com>
2022-08-10 08:07:05 -0700

    finished script to explore climate_distribution

--- code/climate_distribution.R
+++ code/climate_distribution.R
@@ -8,6 +8,8 @@ library(readxl)
 library(lubridate)
 library(dplyr)
 library(ggplot2)
+library(stats)
+library(Hmisc)
 
 # load in data
 S1 <- read_csv("../data/temperature_season_1.csv")
@@ -57,10 +59,29 @@ ggplot(data = incubation_temps,
   facet_wrap(~ Season, ncol = 1)
 
 # mean and median values
-incubation_temps %>%
+all_incubation_temps <- incubation_temps %>%
+  filter(Season != 1) %>%
   group_by(Month) %>%
-  summarize(Mean = mean(Temperature), 
-            Median = median(Temperature))
+  dplyr::summarize(Mean = mean(Temperature), 
+                   Median = median(Temperature), 
+                   Variance = var(Temperature))
+
+S2_incubation_temps <- incubation_temps %>%
+  filter(Season == 2) %>%
+  group_by(Month) %>%
+  dplyr::summarize(Mean = mean(Temperature), 
+                   Median = median(Temperature), 
+                   Variance = var(Temperature))
+
+S3_incubation_temps <- incubation_temps %>%
+  filter(Season == 3) %>%
+  group_by(Month) %>%
+  dplyr::summarize(Mean = mean(Temperature), 
+                   Median = median(Temperature), 
+                   Variance = var(Temperature))
+
+# copy S3 incubation temps for S2 month 6
+S2_incubation_temps[6, ] <- S3_incubation_temps[6, ]
 
 # very slight skew, but overall very symmetric - will use median
 
@@ -69,10 +90,109 @@ load("../data/nests.Rdata")
 
 # determine proportion of nests with incubation temperatures per month
 
+# create function to calculate mode
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
 # determine dates of incubation for nests
 nests <- nests %>%
   filter_at(vars(DATA_OCORR, DATA_ECLOS), all_vars(!is.na(.))) %>%
-  # group_by(Season, N_NINHO) %>%
   mutate(Incubation_start = DATA_OCORR + days(floor(as.numeric(date(DATA_ECLOS) - date(DATA_OCORR)) / 3)), 
-         Incubation_end = DATA_OCORR + days(ceiling(as.numeric(date(DATA_ECLOS) - date(DATA_OCORR)) * 2 / 3))) %>%
+         Incubation_end = DATA_OCORR + days(ceiling(as.numeric(date(DATA_ECLOS) - date(DATA_OCORR)) * 2 / 3)))
 
+# pull out most common month from each incubation period
+for (i in 1:nrow(nests)) {
+  
+  incubation  <- seq(from = nests$Incubation_start[i], 
+                     to = nests$Incubation_end[i], 
+                     by = 'days')
+  
+  months <- format(incubation, format = '%m')
+  
+  nests$Key_month[i] <- getmode(as.numeric(months))
+  
+}
+
+# pull out proportion of nests with peak incubation in each month (S2 and S3 only)
+all_proportions <- nests %>%
+  filter(Season != 2020) %>%
+  group_by(Key_month) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n))
+
+S2_proportions <- nests %>%
+  filter(Season == 2021) %>%
+  group_by(Key_month) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n))
+
+S3_proportions <- nests %>%
+  filter(Season == 2022) %>%
+  group_by(Key_month) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n))
+
+# quick check that proportions add up to 1 - good to go
+# sum(S2_proportions$freq)
+# sum(S3_proportions$freq)
+
+# calculate weighted mean for S2 and S3
+all_mean_temp <- weighted.mean(x = all_incubation_temps$Mean, 
+                               y = all_proportions$freq)
+
+# Means
+# 31.54639
+
+# Medians
+# 31.4195
+
+# calculate weighted mean for S2
+S2_mean_temp <- weighted.mean(x = S2_incubation_temps$Mean, 
+                              y = S2_proportions$freq)
+# Means
+# 32.04623
+
+# Medians
+# 31.90867
+
+# calculate weighted mean for S2
+S3_mean_temp <- weighted.mean(x = S3_incubation_temps$Mean, 
+                              y = S3_proportions$freq)
+
+# Means
+# 31.44746
+
+# Medians
+# 31.3
+
+# TODO
+# calculate weighted standard deviation
+
+# all seasons
+all_sd_temp <- sd(incubation_temps$Temperature)
+
+# 1.433039
+
+season_temps <- incubation_temps %>%
+  filter(Season != 1) %>%
+  ungroup() %>%
+  summarise(sd = sd(Temperature))
+
+# 1.42
+
+# season 2
+season_temps <- incubation_temps %>%
+  group_by(Season) %>%
+  summarise(sd = sd(Temperature))
+
+# season 1 - 1.01
+# season 2 - 1.31
+# season 3 - 1.37
+
+################################################################################
+
+# overall incubation temperatures mean for S2 and S3: 31.54639
+
+# overall incubation temperatures sd for S2 and S3: 1.42
