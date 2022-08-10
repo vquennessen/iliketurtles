0c3dfb53bfc9af71cbb009e77a19d5765331d44b
vquennessen <viquennessen@gmail.com>
2022-08-10 08:05:29 -0700

    re-did reproduction function, still troubleshooting the whole arrays dropping dimensions thing

--- code/reproduction.R
+++ code/reproduction.R
@@ -1,22 +1,62 @@
 # reproduction
 
-reproduction <- function(no_betas, betas, temp_mu, temp_var, pivotal_temp, 
-                         N, t) {
-
-  # for each potential mating function
-  for (b in 1:no_betas) {
+reproduction <- function(N, age_maturity, max_age, years, t, betas, b, 
+                         scenarios, s, remigration_int, nests_mu, nests_sd,
+                         eggs_mu, eggs_sd, hatch_success,
+                         temp, temp_sd, climate_stochasticity) {
+  
+  # extract breeding adults from N
+  
+  # females only breed every remigration_int years
+  n_breeding_F <- floor(sum(N[1, age_maturity:max_age, t, b, s], na.rm = TRUE) / remigration_int)
+  
+  # males mate every year???
+  n_breeding_M <- floor(sum(N[2, age_maturity:max_age, t, b, s], na.rm = TRUE))
+  
+  # proportion of males
+  prop_males <- n_breeding_M / (n_breeding_M + n_breeding_F)
+  
+  # relate prop_males to breeding success via mating function
+  breeding_success <- pbeta(prop_males, shape1 = 1, shape2 = betas[b])
+  
+  # number of nests per female
+  nests <- round(rnorm(n = n_breeding_F, mean = nests_mu, sd = nests_sd))
+  
+  # number of eggs per nest
+  DF <- data.frame(Nests = nests, 
+                   Eggs = rep(NA, times = length(nests)))
+  
+  for (n in 1:length(nests)) {
     
-    beta <- betas[b]
-    
-    # for each temperature scenario 
-    for (t in 1:length(temp_mu)) {
-      
-      
-      
-      
-    }
-    
+    DF$Eggs[n] <- sum(round(rnorm(n = DF$Nests[n], 
+                                  mean = eggs_mu, 
+                                  sd = eggs_sd)))
     
   }
+  
+  # total hatchlings = total eggs * hatching success * breeding_success
+  hatchlings <- sum(DF$Eggs) * hatch_success[t, b, s] * breeding_success
+  
+  # for current temperature 
+  if (climate_stochasticity == TRUE) {
+    temperature <- rnorm(mean = temp, sd = temp_sd)
+  } else {
+    temperature <- temp
+  }
+  
+  # determine proportion of male hatchlings based on temperature
+  prop_male <- exp(logit_a + logit_b*temperature) / (1 + exp(logit_a + logit_b*temperature))
+  
+  # number of male and female hatchlings
+  female_hatchlings <- round(hatchlings * (1 - prop_male))
+  male_hatchlings <- round(hatchlings * prop_male)
+  
+  # add to population size array
+  N[1, 1, t + 1, b, s] <- female_hatchlings
+  N[2, 1, t + 1, b, s] <- male_hatchlings
+  
+  output <- list(N)
+  
+  return(output)
   
 }
