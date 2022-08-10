c51fee15702f4233270dfaa1c20a50cf8da5f56b
vquennessen <viquennessen@gmail.com>
2022-08-10 08:07:23 -0700

    created base_model script

--- code/base_model.R
+++ code/base_model.R
@@ -1,62 +1,74 @@
 # base model
 
-# set working directory
-setwd("~/Projects/iliketurtles/code")
-
-##### parameters ###############################################################
-
-# turtle demographics
-max_age <- 85                                         # lifespan
-F_survival_years <- c(1, 2, 7, 12, 1)                 # years per stage - F
-F_survival_values <- c(0.35, 0.8, 0.85, 0.85, 0.929)  # survival per stage - F
-M_survival_years <- c(1, 2, 7, 12, 1)                 # years per stage - M
-M_survival_values <- c(0.35, 0.8, 0.85, 0.85, 0.490)  # survival per stage - M
-betas <- c(1, 10, 100)                                # mating function beta
-pivotal_temp <- 29.3                                  # pivotal temperature 
-TRT <- c(27.6, 31.4)                                  # TR temperatures
-logit_a <- 41.362228                                  # temp -> prop. male a
-logit_b <- -1.415462                                  # temp -> prop. male b
-
-
-# climate data
-temp_mu <- x
-temp_sd <- y
-
-# model parameters
-years <- 200                                      # years to run model  
-scenarios <- c('Same')                            # climate scenarios
-
-
-##### source initialized arrays ################################################
-
-init_output <- initialize_arrays(max_age, years, betas, scenarios, 
-                                 pivotal_temp, TRT, temp_mu, temp_sd, 
-                                 F_survival_years, F_survival_values, 
-                                 M_survival_years, M_survival_values)
-
-temperatures <- init_output[[1]] # temperatures across climate scenarios
-prop_male <- init_output[[2]]    # proportion male by temperature 
-N <- init_output[[2]]            # population size array
-F_survival <- init_output[[3]]   # vector of survival values - females
-M_survival <- init_output[[4]]   # vector of survival values - males
-
-##### model ####################################################################
-
-for (t in 1:years) {
+base_model <- function(max_age, F_survival_years, F_survival_values, 
+                       M_survival_years, M_survival_values, 
+                       betas, remigration_int, nests_mu, nests_sd,
+                       eggs_mu, eggs_sd, hatch_success, 
+                       pivotal_temp, TRT, logit_a, logit_b, 
+                       temp_mu, temp_sd, climate_stochasticity, 
+                       start_year, end_year, scenarios, num_sims) {
   
-  # population dynamics
-  pop_output <- pop_dynamics(F_survival, M_survival, t) 
+  ##### source initialized arrays ##############################################
   
-  N <- init_output[[1]]            # population size array
+  init_output <- initialize_arrays(max_age, start_year, end_year, betas, 
+                                   scenarios, num_sims, temp_mu, temp_sd, 
+                                   F_survival_years, F_survival_values, 
+                                   M_survival_years, M_survival_values)
   
-  # climate change temperature estimates
-  temp_output <- temperature()
+  ages <- init_output[[1]]           # ages
+  years <- init_output[[2]]          # years to run model
+  hatch_success <- init_output[[3]]  # array of hatching success values
+  temperatures <- init_output[[4]]   # temperatures across climate scenarios
+  N <- init_output[[5]]              # population size array
+  F_survival <- init_output[[6]]     # vector of survival values - females
+  M_survival <- init_output[[7]]     # vector of survival values - males
   
-  temp_mu <- output[[1]]           # temperature mean
-  temp_var <- output[[2]]          # temperature variance
+  ##### model ##################################################################
   
-  # reproduction
+  for (t in 1:y) {
+    
+    for (b in 1:length(betas)) {
+      
+      for (s in 1:num_scenarios) {
+        
+        # population dynamics
+        # survival for each age 
+        for (a in 2:max_age) {
+          
+          # annual survival - females
+          N[1, a, t + 1, , ] <- F_survival * N[1, a - 1, t, b, s]
+          
+          # annual survival - males
+          N[2, a, t + 1, , ] <- M_survival * N[2, a - 1, t, b, s]
+          
+        }
+        
+        # climate change temperature estimates
+        temp <- temperatures[t]
+        
+        # reproduction
+        rep_output <- reproduction(N, age_maturity, max_age, years, t, betas, b, 
+                                   scenarios, s, remigration_int, nests_mu, 
+                                   nests_sd, eggs_mu, eggs_sd, hatch_success,
+                                   temp, temp_sd, climate_stochasticity) 
+        
+        N <- rep_output[[1]]
+        
+      }
+      
+    }
+    
+  }
   
+  ##### output #################################################################
+  
+  # create abundance array
+  abundance <- colSums(N, dims = 2)
+  
+  # output N and abundance arrays
+  output <- list(N, abundance)
+  
+  return(output)
   
 }
 
