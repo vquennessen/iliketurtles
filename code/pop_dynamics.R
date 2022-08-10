f8c68d20d0ea8131e67c0760d8a1e56647557810
vquennessen <viquennessen@gmail.com>
2022-08-10 08:05:53 -0700

    trimmed pop_dynamics function down, might eliminate it since it's so small (< 20 lines of code)

--- code/pop_dynamics.R
+++ code/pop_dynamics.R
@@ -1,20 +1,15 @@
 # population dynamics
 
-pop_dynamics <- function(F_survival, M_survival, t) {
+pop_dynamics <- function(max_age, t, b, s) {
   
-  # for each mating function beta value
-  for (b in 1:no_betas) {
+  # for each age 
+  for (a in 2:max_age) {
     
-    # for each climate change scenario
-    for (s in 1:no_scenarios) {
-      
-      # annual survival - females
-      N[1, , t + 1, , ] <- F_survival * N[1, , t, b, s]
-      
-      # annual survival - males
-      N[2, , t + 1, , ] <- M_survival * N[2, , t, b, s]
-      
-    }
+    # annual survival - females
+    N[1, a, t + 1, , ] <- F_survival * N[1, a - 1, t, b, s]
+    
+    # annual survival - males
+    N[2, a, t + 1, , ] <- M_survival * N[2, a - 1, t, b, s]
     
   }
   
