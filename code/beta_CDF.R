2ba163e6305e5a27e7bb29a213463d0e4e666a47
vquennessen <viquennessen@gmail.com>
2022-08-16 20:51:03 -0700

    updated beta_CDF to make figure of hypothetical mating functions

--- code/beta_CDF.R
+++ code/beta_CDF.R
@@ -7,7 +7,7 @@ library(ggplot2)
 x <- seq(from = 0, to = 1, by = 0.01)
 
 # beta values to cycle through
-betas <- c(1:5, 10, 25, 100)
+betas <- c(1, 2, 3, 5, 10, 20, 100)
 
 # initialise DF
 DF <- data.frame(Breeding_Sex_Ratio = rep(x, times = length(betas)), 
@@ -29,4 +29,12 @@ DF$Beta <- as.factor(DF$Beta)
 # plot
 ggplot(data = DF, aes(x = Breeding_Sex_Ratio, y = Reproductive_Success, 
                       color = Beta)) +
-  geom_line(size = 1.25)
+  geom_line(size = 1.25) +
+  theme(text = element_text(size = 15), 
+        axis.title.y = element_text(margin = margin(r = 15, l = 10)), 
+        axis.title.x = element_text(margin = margin(t = 15, b = 10)), 
+        plot.title = element_text(margin = margin(b = 10, t = 10))) +
+  ylab('Reproductive Success') +
+  xlab('Breeding Sex Ratio') +
+  ggtitle('Hypothetical mating functions')
+
