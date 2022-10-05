17bf75cf1c6b4f26865f4ec998f7c4be0f63950f
vquennessen <viquennessen@gmail.com>
2022-10-05 14:23:22 -0700

    still working on grib data

--- code/climate_regression.R
+++ code/climate_regression.R
@@ -8,12 +8,20 @@
 # set working directory
 setwd('~/Projects/iliketurtles')
 
-library(rNOMADS)
+# load libraries
+# library(rNOMADS)
 library(raster)
 
 # load in pre-2022 data
-ReadGrib(file.names = 'data/climate_data_before_2022.grib', 
-         file.type = 'grib1')
+# ReadGrib(file.names = 'data/climate_data_before_2022.grib', 
+#          file.type = 'grib1')
 
+# import objects as RasterBricks using raster package
 GRIB <- brick('data/climate_data_before_2022.grib')
-GRIB <- brick('data/climate_data_2022.grib')
+GRIB2 <- brick('data/climate_data_2022.grib')
+
+# from rasterbrick to dataframe
+before2022 <- raster::as.data.frame(GRIB, xy = FALSE, long = TRUE)
+during2022 <- raster::as.data.frame(GRIB2, xy = FALSE, long = TRUE)
+
+RTP <- rasterToPoints(GRIB)
