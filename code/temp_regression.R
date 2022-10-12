309d005575f4a75f6263080437ed99007defe69d
vquennessen <viquennessen@gmail.com>
2022-10-12 11:00:20 -0700

    adding in ARMA correlation

--- code/temp_regression.R
+++ code/temp_regression.R
@@ -2,6 +2,8 @@
 
 # sources
 # random effects in nlme: https://biostatmatt.com/archives/2718
+# correlations in nlme: section 5.3 in Mixed-Effects Models in S and S-PLUS 
+#     and http://plantecology.syr.edu/fridley/bio793/mixed2.html
 
 # set working directory
 setwd('~/Projects/iliketurtles')
@@ -29,7 +31,7 @@ summary(mod2)
 # plot(mod2)
 
 # random effect for s.nest
-m1 <- lme(nest_temp ~ temp2m + SST, 
+m1 <- lme(nest_temp ~ temp2m + SST + incubation.day, 
           random = ~ 1 | s.nest,
           data = temps)
 summary(m1)
@@ -41,5 +43,22 @@ m2 <- lme(nest_temp ~ temp2m + SST,
           data = temps, 
           random = ~ 1 | Season/Nest)
 summary(m2)
+# AIC      BIC      logLik
+# 1054080  1054143  -527034
+
+# ARMA model = mixed autoregressive-moving average model (for temporal 
+# correlation)
+
+# ARMA correlation
+ARMA <- corARMA(value = c(0.2, 0.2),         # just starting value
+                form = ~ date_time,          # correlation by date_time
+                p = 1, q = 1)                # for ARMA correlation
+
+# random effect for nest nested within season
+m3 <- lme(nest_temp ~ temp2m + SST + incubation.day, # formula
+          data = temps,                              # dataframe
+          random = ~ 1 | Season / Nest,              # random effect of nest
+          correlation = ARMA)                        # ARMA correlation   
+summary(m3)
 # AIC      BIC      logLik
 # 1054080  1054143  -527034
