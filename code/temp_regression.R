2caf41462b0bdc7ad1af483f2a8633897d533a33
vquennessen <viquennessen@gmail.com>
2022-10-19 16:51:18 -0700

    starting with only AR correlation model

--- code/temp_regression.R
+++ code/temp_regression.R
@@ -15,54 +15,57 @@ library(ggplot2)
 # load in data
 load("~/Projects/iliketurtles/data/temperature_data.Rda")
 
-# basic linear regression
-mod1 <- lm(nest_temp ~ temp2m + SST + incubation.day, 
-           data = temps)
-
+##### model 1 ##################################################################
+# random effect for nest nested within season, no autoregressive correlation
+mod1 <- lme(avg_nest_temp ~ avg_air_temp + avg_sst + incubation.prop, 
+          data = temps, 
+          random = ~ 1 | Season / Nest)
 summary(mod1)
-# all very significant correlates, r2 of almost zero
+# AIC       BIC      logLik
+# 20937.92  20984.47 -10461.96
 
-# basic linear regression with datetime
-mod2 <- lm(nest_temp ~ temp2m + SST + date_time + incubation.day, 
-           data = temps)
 
-summary(mod2)
-# still very significant coefficients, but still r2 = 0.05
-# plot(mod2)
-
-# random effect for s.nest
-m1 <- lme(nest_temp ~ temp2m + SST + incubation.day, 
-          random = ~ 1 | s.nest,
-          data = temps)
-summary(m1)
-# AIC      BIC     logLik
-# 1054109  1054161 -527049.4
+##### model 2 ##################################################################
+# just AR model = autoregressive model for temporal correlation
+AR <- corAR1(value = c(0.2),              # just starting value
+             form = ~ date)          # correlation by date_time
 
 # random effect for nest nested within season
-m2 <- lme(nest_temp ~ temp2m + SST + incubation.day, 
-          data = temps, 
-          random = ~ 1 | Season/Nest)
-summary(m2)
-# AIC      BIC      logLik
-# 1054080  1054143  -527034
+mod2 <- lme(avg_nest_temp ~ avg_air_temp + avg_sst + incubation.prop, 
+            data = temps,                              # dataframe
+          random = ~ 1 | Season / Nest,                # random effect of nest
+          correlation = AR)                            # ARMA correlation
+summary(mod2)
+# AIC       BIC       logLik
+# 13838.23  13891.43  -6911.117
 
+# check AR correlation
+ar(temps$avg_nest_temp)
+
+# Coefficients:
+#   1        2        3        4        5  
+# 1.0279  -0.1626   0.0284  -0.0095   0.0283  
+# 
+# Order selected 5  sigma^2 estimated as  0.71
+
+##### model 3 ##################################################################
 # ARMA model = mixed autoregressive-moving average model (for temporal 
 # correlation)
 
-# ARMA correlation
-ARMA <- corARMA(value = c(0.2, 0.2),         # just starting value
-                form = ~ date_time,          # correlation by date_time
-                p = 1, q = 1)                # for ARMA correlation
-
-# random effect for nest nested within season
+# # ARMA correlation
+# ARMA <- corARMA(value = c(0.2, 0.2),         # just starting value
+#                 form = ~ date_time,          # correlation by date_time
+#                 p = 1, q = 1)                # for ARMA correlation
+# 
+# # random effect for nest nested within season
 # m3 <- lme(nest_temp ~ temp2m + SST + incubation.day, # formula
 #           data = temps,                              # dataframe
 #           random = ~ 1 | Season / Nest,              # random effect of nest
-#           correlation = ARMA)                        # ARMA correlation   
+#           correlation = ARMA)                        # ARMA correlation
 # summary(m3)
 # # AIC       BIC      logLik
 # # 1008863   1008958  -504422.7
 # 
 # save(m3, file = "data/m3.Rda")
-
-load(file = "data/m3.Rda")
+# 
+# load(file = "data/m3.Rda")
